3bf93e3f58ffd69c58be4af260d70bb98aea9ec7
yuu735 <yuu981130@gmail.com>
2022-05-05 17:34:46 +0800

    整合elasticsearch版本7！跟es6有所不同

--- src/main/java/com/yuu/community/service/Impl/ElasticsearchService.java
+++ src/main/java/com/yuu/community/service/Impl/ElasticsearchService.java
@@ -1,7 +1,13 @@
 package com.yuu.community.service.Impl;
 
+import com.yuu.community.dao.DiscussDao;
+import com.yuu.community.dao.elasticsearch.DiscussPostRepository;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ElasticsearchService {
+
 }
